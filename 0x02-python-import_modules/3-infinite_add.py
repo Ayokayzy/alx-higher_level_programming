40d7d9209916bcc2937a6d9e9de85ef9cc35432a
Ayokayzy <theayokayzy1@gmail.com>
2023-11-02 22:37:51 +0000

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv)
+    sum = 0
+
+    for i in range(1, argc):
+        sum += int(argv[i])
+
+    print(sum)
