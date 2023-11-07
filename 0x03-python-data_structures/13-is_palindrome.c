3e90ba687d717aaeb42d947d50cf6945b6f71034
Ayokayzy <theayokayzy1@gmail.com>
2023-11-07 22:16:38 +0000

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,6 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-
-size_t num_od_nodes(const listint_t *h)
-{
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
-
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        current = current->next;
-        n++;
-    }
-
-    return (n);
-}
-
-
-/**
  * is_palindrome - a function in C that checks if a
  * singly linked list is a palindrome.
  * @head: address of the first item in the list
@@ -45,6 +22,11 @@ int is_palindrome(listint_t **head)
 	}
 
 	arr = malloc(sizeof(int) * n);
+	if (arr == NULL)
+	{
+		free(arr);
+		return (1);
+	}
 	while(temp != NULL)
 	{
 		arr[i] = temp->n;
@@ -54,8 +36,12 @@ int is_palindrome(listint_t **head)
 	for (i = 0; i < n; i++)
 	{
 		if (arr[i] != arr[n - 1 - i])
+		{
+			free(arr);
 			return (0);
+		}
 	}
+	free(arr);
 	return (1);
 }
 
