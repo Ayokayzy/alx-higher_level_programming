dae82b117e4586241afdde82e20762e3d350397e
Ayokayzy <theayokayzy1@gmail.com>
2023-11-08 01:37:44 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
         for i in range(len(argv) - 1):
             print("{}: {}".format(i + 1, argv[i + 1]))
