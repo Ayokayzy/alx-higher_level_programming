b70b91521f738860de7db78816196998f7245273
Ayokayzy <theayokayzy1@gmail.com>
2023-11-20 22:30:01 +0000

    safe print integer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(0, x):
+    for i in range(0, x):
+        try:
             print("{}".format(my_list[i]), end="")
-        print("")
-    except IndexError:
-        i -= 1
-        print("")
-    finally:
-        return i + 1
+        except IndexError:
+            break
+    return i + 1
