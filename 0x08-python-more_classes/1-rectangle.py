77c227420a6d0ba8e3adbee1d7473cfcf25b3f96
Ayokayzy <theayokayzy1@gmail.com>
2023-12-10 15:44:00 +0000

    Area and Perimeter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -35,10 +35,10 @@ class Rectangle:
 
         if value < 0:
             raise ValueError("width must be >= 0")
-        elif not isinstance(value, int) or not isinstance(value, float):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        else:
-            self.__width = value
+
+        self.__width = value
 
     @property
     def height(self):
@@ -59,8 +59,8 @@ class Rectangle:
 
         if value < 0:
             raise ValueError("height must be >= 0")
-        elif not isinstance(value, int) or not isinstance(value, float):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        else:
-            self.__height = value
+
+        self.__height = value
 
