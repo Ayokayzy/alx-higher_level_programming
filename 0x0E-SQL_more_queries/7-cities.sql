42f6dcfbd302aeb9e8ffab03e81869a74da55ad0
Ayokayzy <theayokayzy1@gmail.com>
2024-01-19 11:34:20 +0000

    Cities by States

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- a script that creates the database hbtn_0d_usa and the table cities
--- (in the database hbtn_0d_usa) on your MySQL serve	r.
+-- (in the database hbtn_0d_usa) on your MySQL server.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS cities (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
