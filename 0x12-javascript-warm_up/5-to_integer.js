526a9e8784c819e2609f1bf292afa55234d88367
Ayokayzy <theayokayzy1@gmail.com>
2024-02-12 19:51:15 +0000

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (String(Number(process.argv[2])) !== 'NaN') {
-  console.log(`My Number: ${Number(process.argv[2])}`);
+  console.log(`My number: ${Number(process.argv[2])}`);
 } else {
   console.log('Not a number');
 }
