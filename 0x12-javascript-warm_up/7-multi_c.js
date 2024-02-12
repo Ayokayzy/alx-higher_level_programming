e7b35a9fb4a49487ed0069d2a8842cfd84660e79
Ayokayzy <theayokayzy1@gmail.com>
2024-02-12 20:44:30 +0000

    Loop to languages

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 if (process.argv[2]) {
   if (Number(process.argv[2]) > 0) {
-    for (i = 0; i < Number(process.argv[2]); i++) {
+    for (let i = 0; i < Number(process.argv[2]); i++) {
 	    console.log('C is fun');
     }
   }
