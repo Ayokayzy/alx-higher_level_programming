5a22a8294492b8fc72adfe00c0d8aac05597e6ea
Ayokayzy <theayokayzy1@gmail.com>
2024-02-14 02:12:45 +0000

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super();
+    this.width = size;
+    this.height = size;
+  }
+
+  charPrint (c) {
+    let square = '';
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      if (square !== '') square += '\n';
+      while (j < this.width) {
+        square += c || 'X';
+        j++;
+      }
+      i++;
+    }
+    console.log(square);
+  }
+}
+
+module.exports = Square;
