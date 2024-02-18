52a60b9580832fe9080d2412576533a16f5738e4
Ayokayzy <theayokayzy1@gmail.com>
2024-02-18 22:02:01 +0000

    update

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+2-my_filter_states.py
+"""
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """
+    lists all states from the database hbtn_0e_0_usa
+    SystemArgs:
+        argv[1]: mysql username,
+        argv[2]: mysql password and
+        argv[3]: database name
+    """
+    db_host = "localhost"
+    db_user = argv[1]
+    db_password = argv[2]
+    db_db = argv[3]
+    db_port = 3306
+
+    query = """SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY id ASC"""
+    try:
+        db = MySQLdb.connect(host=db_host, port=db_port,
+                             user=db_user, passwd=db_password,
+                             db=db_db, charset="utf8")
+        cur = db.cursor()
+        cur.execute(query)
+        states = cur.fetchall()
+
+        for row in states:
+            print(row)
+
+        cur.close()
+        db.close()
+    except Exception:
+        pass
+
+
+if __name__ == "__main__":
+    main()
