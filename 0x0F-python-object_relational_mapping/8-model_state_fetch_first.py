ce827b19ec01c769c13a7455ddd4a68a3a5e97de
Ayokayzy <theayokayzy1@gmail.com>
2024-02-19 01:25:02 +0000

    First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+7-model_state_fetch_all.py
+"""
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(username, password, db_name))
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        state = session.query(State.id, State.name).order_by(State.id).first()
+        if state:
+            print("{}: {}".format(state.id, state.name))
+        else:
+            print("Nothing")
+        session.close()
+    except:
+        pass
