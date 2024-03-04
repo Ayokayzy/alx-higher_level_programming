13534b9b8fcdadd10dcc60d584efb75b3596aa6e
Ayokayzy <theayokayzy1@gmail.com>
2024-03-04 17:49:06 +0000

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+0-hbtn_status.py
+"""
+import urllib.request
+
+
+def main():
+    """
+    a Python script that fetches https://alx-intranet.hbtn.io/status
+    """
+
+    req = urllib.request.Request(
+        "https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        status = response.read()
+        res = "Body response:\n\
+    - type: {}\n\
+    - content: {}\n\
+    - utf8 content: {}".format(type(response.read()), status, response.msg)
+        print(res)
+
+
+if __name__ == "__main__":
+    main()
