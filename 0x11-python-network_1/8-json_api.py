0fe2e92a3feb478ee0a689f8ff6e1206a5bfc5a5
Ayokayzy <theayokayzy1@gmail.com>
2024-03-07 06:23:48 +0000

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,9 +6,18 @@ from sys import argv
 import requests
 
 
-if __name__ == '__main__':
-    req = requests.get(argv[1])
-    if req.status_code >= 400:
-        print('Error code:', req.status_code)
-    else:
-        print(req.text)
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    c = ""
+    if len(argv) > 1:
+        c = {"q": argv[1][0]}
+    req = requests.post(url, data=c)
+    try:
+        res = req.json()
+        if res:
+            str_re = "[{}] {}"
+            print(str_re.format(res.get("id"), res.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
