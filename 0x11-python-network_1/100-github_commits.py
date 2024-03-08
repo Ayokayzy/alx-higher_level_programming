2e35a4d26099a043894554447eca07ac75a81b40
Ayokayzy <theayokayzy1@gmail.com>
2024-03-08 21:31:36 +0000

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+100-github_commits.py
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    repo_name = argv[1]
+    owner_name = argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+
+    try:
+        r = requests.get(url)
+        res = r.json()
+        for i in range(10):
+            print("{}: {}".format(res[i]["sha"],
+                                  res[i].get("commit").
+                                  get("author").get("name")))
+    except Exception:
+        print(Exception)
